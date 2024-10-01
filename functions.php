2300460ab9432234f30f84bfc5fa2b55fa9acc3f
theguffs <youssefabdelmalak01@gmail.com>
2024-10-01 12:26:25 +0200

    milestone 2

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function generatePassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+    $charactersLength = strlen($characters);
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $password;
+}
+?>
